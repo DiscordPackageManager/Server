d8de2e7453559819430f70daa5e33a336dea148b
Max T <maxthakurcodes@gmail.com>
2021-07-25 01:32:02 -0400

    ✨Adding base server code

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import routes from "./routes";
+const app = express();
+const port = 3000;
+
+app.use("/", routes);
+
+app.listen(port, () => {
+  console.log("Started on port " + port);
+});
